f4937698041290e8cb5f417d4c82eb5004c561f8
Silvana Castrillon Zuluaga <silvana.castrillon@outlook.com>
2021-11-06 12:43:04 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Leonardo'));
+app.get('/', (req, res) => res.send('Hola Sabado 6 de Noviembre 2021'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
